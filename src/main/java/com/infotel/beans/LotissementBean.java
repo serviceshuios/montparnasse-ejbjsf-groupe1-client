55873c32833a88e33838f2ccd84d0c1d979733a9
Adam PROUST <adam.proust@outlook.fr>
2019-05-15 15:47:49 +0200

    maj beans

--- src/main/java/com/infotel/beans/LotissementBean.java
+++ src/main/java/com/infotel/beans/LotissementBean.java
@@ -1,13 +1,11 @@
 package com.infotel.beans;
 
-import java.io.Serializable;
-import java.util.List;
+
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
-import com.infotel.ejb.IBanqueRemote;
 import com.infotel.metier.Lotissement;
 import com.infotel.metier.Personne;
 
@@ -18,7 +16,7 @@ public class LotissementBean implements Serializable{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	//private static final long serialVersionUID = 1L;
 	
 	@EJB
 	private ICoeurRemote service;
@@ -27,11 +25,11 @@ public class LotissementBean implements Serializable{
 	
 	private Personne personne = new Personne();
 
-	public IRemote getService() {
+	public ICoeurRemote getService() {
 		return service;
 	}
 
-	public void setService(IRemote service) {
+	public void setService(ICoeurRemote service) {
 		this.service = service;
 	}
 
@@ -51,13 +49,27 @@ public class LotissementBean implements Serializable{
 		this.personne = personne;
 	}
 	
-	public void ajouterLotissement(Lotissement l);
-	public List<Lotissement> listerLotissements();
-	public Lotissement getLotissement(long idLot);
-	public Lotissement getLotissementSuppr(long idLot);
-	public void supprimerLotissement(Lotissement l);
-	public void modifierLotissement(Lotissement l);
+	public void ajouterLotissement() {
+		service.ajouterLotissement(lotissement);
+	}
+	public List<Lotissement> listerLotissements(){
+		return service.listerLotissements();
+	}
+	public Lotissement getLotissement(long idLot) {
+		return service.getLotissement
+	}
+	public Lotissement getLotissementSuppr(long idLot) {
+		
+	}
+	public void supprimerLotissement(Lotissement l) {
+		
+	}
+	public void modifierLotissement(Lotissement l) {
+		
+	}
 	
-	public void acheterLotissement(Personne p, Lotissement l);
+	public void acheterLotissement(Personne p, Lotissement l) {
+		
+	}
 	
 }
