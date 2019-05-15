e58e51658d209f312a83f2adf45393d67d144038
IN-MP-026 <joanainespirescunha@gmail.com>
2019-05-15 16:24:58 +0200

    lotissement bean

--- src/main/java/com/infotel/beans/LotissementBean.java
+++ src/main/java/com/infotel/beans/LotissementBean.java
@@ -18,7 +18,7 @@ public class LotissementBean implements Serializable {
 	/**
 	 * 
 	 */
-	// private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	@EJB
 	private ICoeurRemote service;
