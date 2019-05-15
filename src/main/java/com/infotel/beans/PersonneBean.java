a598f242d1d34deb49dfbd3795ab9059cee9e676
IN-MP-026 <joanainespirescunha@gmail.com>
2019-05-15 16:27:27 +0200

    personne bean

--- src/main/java/com/infotel/beans/PersonneBean.java
+++ src/main/java/com/infotel/beans/PersonneBean.java
@@ -18,7 +18,7 @@ public class PersonneBean implements Serializable{
 	/**
 	 * 
 	 */
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	@EJB
 	private ICoeurRemote service;
